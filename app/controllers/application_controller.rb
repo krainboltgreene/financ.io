cb21359ee969cc8ed48f389adb6dd6133e793885
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-06-06 21:45:03 -0500

    Make sure to handle null accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_account
+    AccountDecorator.new(super || AccountNull.new)
+  end
+
+  def account_signed_in?
+    current_account.is_a? Account
+  end
 end
